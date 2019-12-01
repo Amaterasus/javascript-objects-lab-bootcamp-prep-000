fa6d233452116ac61531714474e105116aad6e0b
Skye Geraghty <skyetay03@googlemail.com>
2019-12-01 13:00:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ function updateObjectWithKeyAndValue(object, key, value)
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
 {
-  
-  return object[key] = value
+  object[key] = value
+  return object
 }
 
 function deleteFromObjectByKey(object, key)
 {
-  
+  var toBeReturned = Object.assign({}, object)
+  delete toBeReturned.key
+  return toBeReturned
 }
 
 function destructivelyDeleteFromObjectByKey(object, key)
